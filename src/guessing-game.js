771414c81f5b20bf258a608c0780be132d98ee22
iryna-zhyhala <iryna.zhyhala@gmail.com>
2023-09-17 15:28:53 +0300

    feat: add solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.medium = Math.round((this.max + this.min) / 2);
+        return this.medium;
     }
 
     lower() {
-
+        this.max = this.medium;
     }
 
     greater() {
-
+        this.min = this.medium;
     }
 }
 
